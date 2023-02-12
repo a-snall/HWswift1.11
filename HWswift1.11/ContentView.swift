008c404172c197607db08437d3dc5157d3b0acf5
Astrid Snäll <vaski2astrid@hotmail.com>
2023-02-12 12:47:26 -0500

    HW1.11 adding images

--- HWswift1.11/ContentView.swift
+++ HWswift1.11/ContentView.swift
@@ -5,22 +5,58 @@
 //  Created by Astrid Snäll on 2/6/23.
 //
 
+
+
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var messageString = "  "
+    @State private var imageName = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
+                .padding()
+            
+            Spacer()
+            
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            
+            
+            Button("Show image") {
+                let message1 = "You're great!"
+                let message2 = "You're awesome!"
+                messageString = (messageString == message1 ? message2
+                                 :message1)
+                imageName = ( imageName == "image0" ? "image1"
+                              : "image0" )
+            }
+            .buttonStyle(.borderedProminent)
+            
+            
+        }}}
+        
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
         }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+        
+            
+            
+   
